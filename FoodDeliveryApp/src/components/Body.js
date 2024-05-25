62cb89f6df1c9f4a59d55b9807feee404725e7b8
shivanip1512 <shivanipacharne10@gmail.com>
2024-05-26 02:32:08 +0530

    React Hooks + State variables

--- FoodDeliveryApp/src/components/Body.js
+++ FoodDeliveryApp/src/components/Body.js
@@ -1,14 +1,33 @@
 import  RestaurantCard  from "./RestaurantCard";
 import restraunts from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+
+    // State variable
+    const [listOfRestaurant, setListOfRestaurant] = useState(restraunts);
+
+    // Normal variable
+    // let listOfRestaurant;
+    
     return (
         <div className="body">
-            <div className="search">Search</div>
+            <div className="filter">
+                <button className="filter-btn" onClick={() => {
+                    // Filter Logic here
+                    const filteredList = listOfRestaurant.filter(
+                        (res) => res.info.avgRating > 4
+                    );
+                    setListOfRestaurant(filteredList);
+                }}>4+ Ratings</button> 
+            </div>
             <div className="res-container">
                 {
-                    restraunts.map(restaurant =>
-                        (<RestaurantCard key={restaurant.info.id} resData={restaurant} />)
+                    listOfRestaurant.map(restaurant =>
+                        (
+                            <RestaurantCard key={restaurant.info.id}
+                                resData={restaurant} />
+                        )
                     )
                 }
             </div>
