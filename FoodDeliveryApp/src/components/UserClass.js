f96017fd6e9d8741748a88fa45b334732b45a472
shivanip1512 <shivanipacharne10@gmail.com>
2024-06-09 22:00:01 +0530

    state variables

--- FoodDeliveryApp/src/components/UserClass.js
+++ FoodDeliveryApp/src/components/UserClass.js
@@ -4,12 +4,19 @@ class UserClass extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            count: 0,
+            count2: 1
+        }
     }
 
     render() {
-        const {name, location} = this.props;
+        const { name, location } = this.props;
+        const {count, count2} = this.state;
         return (
-        <div className="user-card" >
+            <div className="user-card" >
+            <h3>Count = {count}</h3>
+            <h3>Count2 = {count2}</h3>
             <h2>Name : {name}</h2>
             <h3>Location : {location}</h3>
             <h4>Contact : @im_shivanip</h4>
