05fa66c97c2a0eaf9c9524078e5c1a30d80bcd59
shivanip1512 <shivanipacharne10@gmail.com>
2024-07-29 00:17:22 +0530

    Added other actions

--- FoodDeliveryApp/src/components/Cart.js
+++ FoodDeliveryApp/src/components/Cart.js
@@ -1,19 +1,26 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { CDN_RESTAURANT_URL } from "../utils/constants";
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import { clearCart, removeItem } from "../utils/redux/cartSlice";
 
 var totalAmount = 0;
 
 export const Cart = () => {
   const cartItems = useSelector((store) => store.cart.items);
-  console.log(totalAmount);
+  const dispatch = useDispatch();
+  const handleClearCart = () => {
+    dispatch(clearCart());
+  }
     return (
         <section className="bg-white py-8 antialiased dark:bg-gray-900 md:py-16">
           <div className="mx-auto max-w-screen-xl px-4 2xl:px-0">
-            <h2 className="text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl">Checkout Cart</h2>
-          <div className="mt-6 sm:mt-8 md:gap-6 lg:flex lg:items-start xl:gap-8">
-            <div>
+            <div className="flex justify-between">
+              <h2 className="text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl">Checkout Cart</h2>
+              <button type="submit" className="flex w-2/12 items-center justify-center rounded-lg bg-gray-400 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300" onClick={handleClearCart}>Clear Cart</button>
+            </div>
+            <div className="mt-6 sm:mt-8 md:gap-6 lg:flex lg:items-start xl:gap-8">
+              <div>
               {  
                 cartItems.map((item) => <Item key={item.id} data={item} />)
               }
@@ -29,8 +36,13 @@ const Item = ({ data }) => {
   const [itemQuantity, setItemQuantity] = useState(1);
   const price = ( data.price ? data.price : data.defaultPrice) / 100;
   const itemTotalAmount = price * itemQuantity;
-  console.log(totalAmount, itemTotalAmount)
   totalAmount += itemTotalAmount;
+  // console.log(totalAmount, itemTotalAmount)
+
+  const dispatch = useDispatch();
+  const handleRemoveItem = (item) => {
+    dispatch(removeItem(item))
+  };
 
   return (
     <div className="mx-auto w-full pb-5">
@@ -73,7 +85,7 @@ const Item = ({ data }) => {
                     Add to Favorites
                   </button>
 
-                  <button type="button" className="inline-flex items-center text-sm font-medium text-red-600 hover:underline dark:text-red-500">
+                  <button type="button" className="inline-flex items-center text-sm font-medium text-red-600 hover:underline dark:text-red-500" onClick={()=>handleRemoveItem(data)}>
                     <svg className="me-1.5 h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                       <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18 17.94 6M18 18 6.06 6" />
                     </svg>
@@ -92,7 +104,7 @@ const OrderSummary = ({ data }) => {
   const price = data.reduce((n, { price }) => n + price, 0)/100;
   const [savings, setSavings] = useState(0);
   const deliveryCharges = 35;
-  const tax = ((price + deliveryCharges) * 5 / 100).toFixed(2);
+  const tax = parseFloat(((price + deliveryCharges) * 5 / 100).toFixed(2));
   const total = price - savings + deliveryCharges + tax;
   const [couponCode, setCouponCode] = useState();
   
