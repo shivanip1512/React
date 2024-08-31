5a339517d9978552910a7ebd43e53ff77c002245
shivanip1512 <shivanipacharne10@gmail.com>
2024-08-31 21:21:59 +0530

    describe - Grouping test cases

--- FoodDeliveryApp/src/components/__test__/Contact.test.js
+++ FoodDeliveryApp/src/components/__test__/Contact.test.js
@@ -2,26 +2,30 @@ import { render, screen } from "@testing-library/react"
 import Contact from "../Contact"
 import "@testing-library/jest-dom"
 
-test("test load of Contact Us Component", () => {
+describe("Contact Us Component test cases", () => {
+
+test("test load of Component", () => {
     render(<Contact />);
     const heading = screen.getByRole("heading");
     expect(heading).toBeInTheDocument();
 });
 
-test("test load of button inside Contact Us Component", () => {
+test("test load of button inside Component", () => {
     render(<Contact />);
     const button = screen.getByRole("button");
     expect(button).toBeInTheDocument();
 });
 
-test("test load of button inside Contact Us Component", () => {
+test("test load of Text inside Component", () => {
     render(<Contact />);
     const msg = screen.getByText("Your message");
     expect(msg).toBeInTheDocument();
 });
 
-test("test to load 3 input text boxes inside Contact US component", () => {
+test("test to load 3 input text boxes inside Component", () => {
     render(<Contact />);
     const inputs = screen.getAllByRole("textbox");
     expect(inputs.length).toBe(3);
+});
+
 });
